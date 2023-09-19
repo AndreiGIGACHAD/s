43b08e5734cf963b58978b2b4e54aa0810f64d19
bratu <bratunbratka@gmail.com>
2023-09-19 12:55:39 +0500

    1

--- src/main/java/com/example/test_reset_service/controller/MyController.java
+++ src/main/java/com/example/test_reset_service/controller/MyController.java
@@ -1,7 +1,11 @@
 package com.example.test_reset_service.controller;
 
 
+import com.example.test_reset_service.service.ModifyRequestService;
+import com.example.test_reset_service.service.MyModifyService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,23 +13,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.test_reset_service.model.Request;
 import com.example.test_reset_service.model.Response;
-    @Slf4j
-    @RestController
-    public class MyController {
-        @PostMapping(value = "/feedback")
-        public ResponseEntity<Response> feedback(@RequestBody Request request) {
-            log.info("Входящий request : " + String.valueOf(request));
-            Response response = Response.builder()
-                    .uid(request.getUid())
-                    .operationUid(request.getOperationUid())
-                    .systemTime(request.getSystemTime())
-                    .code("success")
-                    .errorCode("")
-                    .errorMessage("")
-                    .build();
-           //Response responseAfterModify = MyModifyService.modify(response);
-            log.info("Исходящий response :" + String.valueOf(response));
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
 
+@Slf4j
+@RestController
+public class MyController {
+    private final MyModifyService myModifyService;
+    private final ModifyRequestService modifyRequestService;
+
+    @Autowired
+    public MyController(@Qualifier("ModifySystemTime") MyModifyService myModifyService, ModifyRequestService modifyRequestService, ModifyRequestService modifyRequestService1) {
+
+
+        this.myModifyService = myModifyService;
+
+        this.modifyRequestService = modifyRequestService;
     }
+    @PostMapping (value="/feedback")
+    public ResponseEntity<Response> feedback (@RequestBody Request request) {
+
+     log.warn("Вxодяший гequest : " + String.valueOf(request));
+        Response response = Response.builder()
+            .uid(request.getUid())
+            .operationUid (request.getOperationUid())
+            .systemTime(request.getSystemTime())
+            .code("success")
+                     .errorCode("")
+                     .errorMessage("")
+                     .build();
+modifyRequestService.modifyRq(request);
+    Response responseAfterModify = myModifyService.modify(response);
+    log.warn("Исxодяший response: " + String.valueOf(response));
+    return new ResponseEntity<> (responseAfterModify, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/test_reset_service/service/ModifyRequestService.java
@@ -0,0 +1,10 @@
+package com.example.test_reset_service.service;
+
+import com.example.test_reset_service.model.Request;
+
+public interface ModifyRequestService {
+
+
+    void modifyRq(Request request);
+}
+
